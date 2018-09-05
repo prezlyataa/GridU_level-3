2903ca78e28b224471c136db64c6faf22e699e07
Yuriy Prezlyata <yprezlyata@yprezlyata2-wf.local>
2018-09-05 12:18:48 +0300

    added to route not found page

--- src/app/routes/routeSchema.js
+++ src/app/routes/routeSchema.js
@@ -4,6 +4,7 @@ import { LoginPage } from '../components/LoginPage';
 import { SecondPage } from '../components/SecondPage';
 import { ProductsPage } from '../components/ProductsPage';
 import { ProductDetailsPage } from '../components/ProductDetailsPage';
+import { NotFoundPage } from '../components/NotFoundPage';
 
 export class RouteSchema extends Component {
     render() {
@@ -11,7 +12,8 @@ export class RouteSchema extends Component {
             <Route key='login' exact path='/' component={ LoginPage }/>,
             <Route key='productspage' path='/productsPage' component={ ProductsPage }/>,
             <Route key='productdetailspage' path='/productDetailsPage' component={ ProductDetailsPage }/>,
-            <Route key='secondpage' path='/secondPage' component={ SecondPage }/>
+            <Route key='secondpage' path='/secondPage' component={ SecondPage }/>,
+            <Route key='notfoundpage' path='*' component={ NotFoundPage } />
         ];
     }
 }
--- src/services/httpService.js
+++ src/services/httpService.js
@@ -40,3 +40,38 @@ class HttpService {
 }
 
 export default HttpService;
+
+
+// import axios from 'axios'
+//
+// const BASE_URL = 'http://localhost:3000/api'
+//
+// const successHandler = response => response.data
+// const errorHandler = error => console.log(error)
+//
+// export const generateHttpHandler =
+//   (handler, onSuccess = successHandler, onError = errorHandler) =>
+//     handler.then(onSuccess).catch(onError)
+//
+// export const HttpService = {
+//   get(url, config) {
+//     return generateHttpHandler(axios.get(url, config))
+//   },
+//   post(url, data, config) {
+//     return generateHttpHandler(axios.post(url, data, config))
+//   },
+//   put(url, data, config) {
+//     return generateHttpHandler(axios.put(url, data, config))
+//   },
+//   delete(url, config) {
+//     return generateHttpHandler(axios.delete(url, config))
+//   }
+// }
+//
+// export default {
+//   getAllQuestions: () => HttpService.get(`${BASE_URL}/questions`),
+//   getQuestionById: id => HttpService.get(`${BASE_URL}/questions/${id}`),
+//   updateQuestionDetails: (id, updates) => HttpService.put(`${BASE_URL}/questions/${id}`, updates),
+//   addQuestion: question => HttpService.post(`${BASE_URL}/questions`, question),
+//   deleteQuestion: id => HttpService.delete(`${BASE_URL}/questions/${id}`)
+// }
